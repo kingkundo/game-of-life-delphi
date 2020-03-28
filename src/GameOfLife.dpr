cb682e016354e53553b5d0689f2eba85999fa98d
Tom Taylor <thomas.taylor@admit-one.eu>
2020-03-28 23:02:03 +0000

    Posting of the final v1.0 code

--- src/GameOfLife.dpr
+++ src/GameOfLife.dpr
@@ -18,7 +18,7 @@ begin
     Application.MainFormOnTaskbar := True;
     TStyleManager.TrySetStyle('Metropolis UI Dark');
   {$IFEND}
-  Application.Title := 'Game of life';
+  Application.Title := 'Game of Life';
   Application.CreateForm(TGOLMainForm, MainForm);
   Application.Run;
 end.
